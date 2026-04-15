e86ff717f4c14f1dec7474ab8e79515a7ec0b395
Halloloid <animation12321@gmail.com>
2026-04-15 12:46:19 +0000

    Added Spawn Food

--- src/main.rs
+++ src/main.rs
@@ -46,23 +46,49 @@ fn main() -> io::Result<()> {
         end: Position { x: 50, y: 20 },
     };
 
+    let inside_boundary = Boundary{
+        start:Position { x: boundary.start.x+1, y: boundary.start.y+1 },
+        end:Position { x: boundary.end.x-2, y: boundary.end.y-2 },
+    };
+
+    let mut food_postion = Position{x:0,y:0};
+
     terminal::enable_raw_mode()?;
-    create_boundary(&mut stdout, boundary)?;
+    create_boundary(&mut stdout, &boundary)?;
+    for _ in 1..100{
+        spawn_food(&mut stdout, &mut food_postion, &inside_boundary)?;
+    }
     terminal::disable_raw_mode()?;
+
+    stdout.execute(cursor::MoveTo(0,boundary.end.y))?;
 
     Ok(())
 }
 
-fn create_boundary(stdout: &mut Stdout, boundary: Boundary) -> io::Result<()> {
+fn create_boundary(stdout: &mut Stdout, boundary: &Boundary) -> io::Result<()> {
     for y in boundary.start.y..boundary.end.y {
         for x in boundary.start.x..boundary.end.x {
             if (y == 0 || y == boundary.end.y - 1) || (x == 0 || x == boundary.end.x - 1) {
                 stdout
                     .queue(cursor::MoveTo(x, y))?
-                    .queue(style::PrintStyledContent("██".red()))?;
+                    .queue(style::PrintStyledContent("⏹".red()))?;
             }
         }
     }
+    stdout.flush()?;
+    Ok(())
+}
+
+fn spawn_food(stdout: &mut Stdout,food_postion:&mut Position,boundary: &Boundary) -> io::Result<()> {
+    *food_postion = Position { 
+        x: rand::thread_rng().gen_range(boundary.start.x..boundary.end.x), 
+        y: rand::thread_rng().gen_range(boundary.start.y..boundary.end.y)
+    };
+
+    stdout
+    .queue(cursor::MoveTo(food_postion.x,food_postion.y))?
+    .queue(style::PrintStyledContent("⏹".magenta()))?;
+
     stdout.flush()?;
     Ok(())
 }
